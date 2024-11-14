3a4c4006a49644f4ade43f372793fa7b21311fd2
GrigoryanDav <grigoryand219@gmail.com>
2024-11-14 18:06:01 +0400

    create routing system for my app with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import MainLayout from "./components/layouts/Main";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import { ROUTE_CONSTANTS } from "./core/utils/constants";
+import './styles/global.css'
+
+
 const App = () => {
   return (
     <div>
-      
+      <RouterProvider
+        router={
+          createBrowserRouter(
+            createRoutesFromElements(
+              <Route path="/" element={<MainLayout />}>
+                <Route path={ROUTE_CONSTANTS.LOGIN} element={<Login />} />
+                <Route path={ROUTE_CONSTANTS.REGISTER} element={<Register />} />
+              </Route>
+            )
+          )
+        }
+      />
     </div>
   );
 }
