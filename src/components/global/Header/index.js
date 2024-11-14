be6da4f3779ceadfee98b0fd5d34afcfaabe15b2
GrigoryanDav <grigoryand219@gmail.com>
2024-11-14 19:39:11 +0400

    Add Header and change global styles

---
+++ src/components/global/Header/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const Header = () => {
+    return (
+        <div className="header_container">
+            <h2>Resume Generator</h2>
+        </div>
+    )
+}
+
+export default Header
--- src/components/layouts/Main/index.js
+++ src/components/layouts/Main/index.js
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
+import Header from "../../global/Header"
 
 
 const MainLayout = () => {
     return (
         <div>
+            <Header />
             <main>
                 <Outlet />
             </main>
