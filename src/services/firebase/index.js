c99a21d90c266328ac7d18c1358a1d0dfc8d3024
GrigoryanDav <grigoryand219@gmail.com>
2024-11-14 18:11:40 +0400

    Set up firebase config for user authentification

---
+++ src/services/firebase/index.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHy2WnTq6mxsDIP6Oo88cImAVBes8OOeY",
+  authDomain: "resume-builder-884a9.firebaseapp.com",
+  projectId: "resume-builder-884a9",
+  storageBucket: "resume-builder-884a9.firebasestorage.app",
+  messagingSenderId: "1011129446363",
+  appId: "1:1011129446363:web:6d469e5aeb3eff331e81fe"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+export {
+    auth,
+}
