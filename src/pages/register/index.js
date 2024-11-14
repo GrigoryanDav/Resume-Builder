7871d719cf3d7196ee6789babae66031fd015117
GrigoryanDav <grigoryand219@gmail.com>
2024-11-14 18:32:39 +0400

    Create user and save in firebase-firestore in register

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,13 +1,39 @@
 import { Form, Button, Input, Flex } from "antd"
 import AuthWrapper from "../../components/shared/AuthWrapper"
-import { Link } from "react-router-dom"
-import { ROUTE_CONSTANTS } from "../../core/utils/constants"
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { auth, db } from "../../services/firebase"
+import { setDoc, doc } from "firebase/firestore"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { ROUTE_CONSTANTS, FIRESTORE_PATH_NAMES } from "../../core/utils/constants"
 import registerBanner from '../../core/images/cv-register.webp'
 
 const Register = () => {
+    const [loading, setLoading] = useState(false)
+    const [form] = Form.useForm()
+    const navigate = useNavigate()
+
+    const handleRegister = async (values) => {
+        setLoading(true)
+        const { firstName, lastName, email, password } = values
+        try{
+             const response = await createUserWithEmailAndPassword(auth, email, password)
+             const { uid } = response.user
+             const createdDoc = doc(db, FIRESTORE_PATH_NAMES.REGISTERED_USERS, uid)
+             await setDoc(createdDoc, {
+                uid, firstName, lastName, email
+             })
+             navigate(ROUTE_CONSTANTS.LOGIN)
+        } catch (e) {
+            console.log(e)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
         <AuthWrapper title="Sign Up" banner={registerBanner}>
-            <Form layout='vertical'>
+            <Form layout='vertical' form={form} onFinish={handleRegister}>
                 <Form.Item
                     label="First Name"
                     name="firstName"
@@ -55,7 +81,7 @@ const Register = () => {
                 </Form.Item>
                 <Flex align="center" justify="flex-end" gap='10px'>
                     <Link to={ROUTE_CONSTANTS.LOGIN}>Sign In</Link>
-                    <Button type="primary" htmlType="submit">Sign Up</Button>
+                    <Button type="primary" htmlType="submit" loading={loading}>Sign Up</Button>
                 </Flex>
             </Form>
         </AuthWrapper>
