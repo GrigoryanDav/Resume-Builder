e0c4e3b8c5f3bf82771c1866595d0aa4106ec7a7
GrigoryanDav <grigoryand219@gmail.com>
2024-11-17 19:53:04 +0400

    Add Mini Project Section in Resume Form and add its styles

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import { fetchUserProfileInfo } from "./state-managment/slices/userProfile";
 import EducationSection from "./pages/EducationSection";
 import ProfileSection from "./pages/ProfileSection";
 import SkillsSector from "./pages/SkillsSector";
+import MiniProject from "./pages/MiniProject";
 import './styles/global.css'
 
 
@@ -39,6 +40,7 @@ const App = () => {
                     <Route path={ROUTE_CONSTANTS.EDUCATION_SECTION} element={<EducationSection />} />
                     <Route path={ROUTE_CONSTANTS.PROFILE_SECTION} element={<ProfileSection />} />
                     <Route path={ROUTE_CONSTANTS.SKILLS_SECTOR} element={<SkillsSector />} />
+                    <Route path={ROUTE_CONSTANTS.MINI_PROJECT} element={<MiniProject />}/>
                   </Route>
                 </Route>
               </Route>
--- src/pages/MiniProject/index.js
+++ src/pages/MiniProject/index.js
@@ -1,7 +1,53 @@
+import { Form, Input, Button } from "antd"
+import './index.css'
+
+
 const MiniProject = () => {
     return (
-        <div>
+        <div className="mini_project_container">
+            <h3>Add your Mini Projects</h3>
+            <Form>
+                <Form.Item
+                    name='projectName'
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Project Name'
+                        }
+                    ]}
+                >
+                    <Input type="text" placeholder="Project Name"/>
+                </Form.Item>
 
+                <Form.Item
+                    name='techStack'
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Tech Stack'
+                        }
+                    ]}
+                >
+                        <Input type="text" placeholder="Tech Stack"/>
+                </Form.Item>
+
+                <Form.Item
+                    name='description'
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Description'
+                        }
+                    ]}
+                >
+                    <Input type="text" placeholder="Description"/>
+                </Form.Item>
+            </Form>
+
+            <div>
+                <Button>DELETE</Button>
+                <Button type="primary">ADD PROJECT</Button>
+            </div>
         </div>
     )
 }
