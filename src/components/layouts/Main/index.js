83ff9faa7b3ffc05461af193aca71834efe48761
GrigoryanDav <grigoryand219@gmail.com>
2024-11-17 20:15:16 +0400

    Add redirecting from MainLayout to Login or Cabinet

--- src/components/layouts/Main/index.js
+++ src/components/layouts/Main/index.js
@@ -1,8 +1,22 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useEffect } from "react"
+import { ROUTE_CONSTANTS } from "../../../core/utils/constants"
+import { useSelector } from "react-redux"
 import Header from "../../global/Header"
 
 
 const MainLayout = () => {
+    const navigate = useNavigate()
+    const { authUserInfo: { isAuth } } = useSelector((store) => store.userProfile)
+
+    useEffect(() => {
+        if(!isAuth) {
+            navigate(ROUTE_CONSTANTS.LOGIN)
+        } else {
+            navigate(ROUTE_CONSTANTS.CABINET)
+        }
+    })
+
     return (
         <div>
             <Header />
