29960e7ea1ba49b2d9e71e15e5286ea48142b843
GrigoryanDav <grigoryand219@gmail.com>
2024-11-17 18:56:49 +0400

    Add rules for Profile Section and structure for Education Section

--- src/pages/EducationSection/index.js
+++ src/pages/EducationSection/index.js
@@ -1,7 +1,68 @@
+import { Form, Input, Button, Flex } from "antd"
+
 const EducationSection = () => {
+    const [form] = Form.useForm()
     return (
-        <div>
-            
+        <div className="education_section_container">
+            <h3>Add your Education</h3>
+            <Form form={form}>
+                <Flex>
+                    <Form.Item
+                        name='courseName'
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Course Name'
+                            }
+                        ]}
+                    >
+                        <Input type="text" placeholder="Course Name" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name='college-School'
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your College or School'
+                            }
+                        ]}
+                    >
+                        <Input type="text" placeholder="College/School"></Input>
+                    </Form.Item>
+                </Flex>
+
+                <Flex>
+                    <Form.Item
+                        name='completionYear'
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Completion Year'
+                            }
+                        ]}
+                    >
+                        <Input type="number" placeholder="Completion Year" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name='percentage'
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Percentage'
+                            }
+                        ]}
+                    >
+                        <Input type="text" placeholder="Percentage" />
+                    </Form.Item>
+                </Flex>
+            </Form>
+
+            <Flex>
+                <Button type="primary">DELETE</Button>
+                <Button type="primary">ADD EDUCATION</Button>
+            </Flex>
         </div>
     )
 }
--- src/pages/ProfileSection/index.js
+++ src/pages/ProfileSection/index.js
@@ -3,35 +3,66 @@ import "./index.css"
 
 
 const ProfileSection = () => {
+    const [form] = Form.useForm()
     return (
         <div className="profile_section_container">
             <h3>Add your profile details</h3>
-            <Form>
+            <Form form={form}>
                 <div>
                     <Form.Item
                         name='firstName'
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your First Name'
+                            }
+                        ]}
                     >
                         <Input placeholder='First Name' />
                     </Form.Item>
                     <Form.Item
                         name='lastName'
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Last Name'
+                            }
+                        ]}
                     >
                         <Input placeholder="Last Name" />
                     </Form.Item>
                     <Form.Item
                         name='profileImage'
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Profile Image URL'
+                            }
+                        ]}
                     >
-                        <Input type="text" placeholder="Image Url"/>
+                        <Input type="text" placeholder="Image Url" />
                     </Form.Item>
                 </div>
                 <div>
                     <Form.Item
                         name='phoneNumber'
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please nput your Phone Number'
+                            }
+                        ]}
                     >
                         <Input placeholder="Phone Number" type="number" />
                     </Form.Item>
                     <Form.Item
                         name='adress'
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please Input your Adress'
+                            }
+                        ]}
                     >
                         <Input placeholder="Adress" />
                     </Form.Item>
