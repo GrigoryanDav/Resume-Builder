b9984c4875c8fdf2b99f8fa8e962c50447a25847
GrigoryanDav <grigoryand219@gmail.com>
2024-11-19 21:05:44 +0400

    Add add button and delete button functionality in Social Section

--- src/pages/Social/index.js
+++ src/pages/Social/index.js
@@ -1,28 +1,65 @@
 import { Form, Input, Button } from "antd"
+import { useState } from "react"
 import './index.css'
 
 const Social = () => {
     const [form] = Form.useForm()
+    const [socials, setSocials] = useState([{}])
+    const [isDeleteEnabled, setIsDeleteEnabled] = useState(false)
+
+    const addSocial = () => {
+        const newSocialFields = [...socials, {}]
+        setSocials(newSocialFields)
+        if (newSocialFields.length > 1) {
+            setIsDeleteEnabled(true)
+        }
+    }
+
+    const deleteSocials = () => {
+        const newSocialFields = socials.slice(0, -1)
+        setSocials(newSocialFields)
+        if (newSocialFields.length < 2) {
+            setIsDeleteEnabled(false)
+        }
+    }
+
     return (
         <div className="social_container">
             <h3>Add social links like linkedin , github etc</h3>
             <Form form={form}>
-                <Form.Item
-                    name='socialLinks'
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your Social Links'
-                        }
-                    ]}
-                >
-                    <Input type="text" placeholder="Social Links" />
-                </Form.Item>
+                {
+                    socials.map((_, index) => (
+                        <div key={index}>
+                            <Form.Item
+                                name={['social', index, 'socialLinks']}
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your Social Links'
+                                    }
+                                ]}
+                            >
+                                <Input type="text" placeholder="Social Links" />
+                            </Form.Item>
+                        </div>
+                    ))
+                }
             </Form>
 
             <div>
-                <Button>DELETE SOCIAL</Button>
-                <Button type="primary">ADD SOCIAL</Button>
+                <Button
+                    onClick={deleteSocials}
+                    disabled={!isDeleteEnabled}
+                >
+                    DELETE SOCIAL
+                </Button>
+
+                <Button
+                    type="primary"
+                    onClick={addSocial}
+                >
+                    ADD SOCIAL
+                </Button>
             </div>
         </div>
     )
