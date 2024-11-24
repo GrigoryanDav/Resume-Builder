ea2e85ca914e60ba8dd34d2b1f7828f295bd858d
GrigoryanDav <grigoryand219@gmail.com>
2024-11-24 23:15:51 +0400

    Add styles for second and third resumes, finished third resume html structure

--- src/components/shared/UserResume-3/index.js
+++ src/components/shared/UserResume-3/index.js
@@ -1,7 +1,50 @@
-const  ThirdResume = () => {
+import { useSelector } from "react-redux"
+import { FacebookOutlined, GithubOutlined, PhoneOutlined, EnvironmentOutlined } from '@ant-design/icons'
+import { showEducations, showMiniProjects, showSkills } from "../../../core/helpers"
+import './index.css'
+
+const ThirdResume = () => {
+    const { authUserInfo: { userData: { resume_sections: { profile, education, skills: { skills }, social, miniProject } } } } = useSelector((store) => store.userProfile)
     return (
-        <div>
-            <h2>Hello World</h2>
+        <div className="third_resume_container">
+            <div className="third_left_side">
+                <div className="third_img_container">
+                    <img src={profile.profileImage} alt="profile-image" />
+                </div>
+                <div className="third_left_information">
+                    <h1>{profile.firstName} {profile.lastName}</h1>
+                    <div className="third_contact">
+                        <h1>Contact</h1>
+                        <div>
+                            <h3><span><PhoneOutlined /></span>{profile.phoneNumber}</h3>
+                            <h3><span><EnvironmentOutlined /></span>{profile.adress}</h3>
+                        </div>
+                    </div>
+                    <div className="third_social">
+                        <h1>Social</h1>
+                        <div>
+                            <a href={social.social_facebook} target="_blank" rel="noreferrer"><FacebookOutlined />  FACEBOOK</a>
+                            <a href={social.social_github} target="_blank" rel="noreferrer"><GithubOutlined />  GITHUB</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="third_right_side">
+                <div className="third_education">
+                    <h1>Education</h1>
+                    {showEducations(education)}
+                </div>
+                <div className="third_skills">
+                    <h1>Skills</h1>
+                    <ul>
+                        {showSkills(skills)}
+                    </ul>
+                </div>
+                <div className="third_miniProject">
+                    <h1>Mini Project</h1>
+                    {showMiniProjects(miniProject)}
+                </div>
+            </div>
         </div>
     )
 }
