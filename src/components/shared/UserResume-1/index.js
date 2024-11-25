0cc51c981b25ec91b2941732ebf5199b5b775236
GrigoryanDav <grigoryand219@gmail.com>
2024-11-25 17:02:55 +0400

    Add media styles for user resume templates

--- src/components/shared/UserResume-1/index.js
+++ src/components/shared/UserResume-1/index.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux"
 import { showEducations, showMiniProjects, showSkills } from "../../../core/helpers"
-import { GithubOutlined, FacebookOutlined, PhoneOutlined, EnvironmentOutlined, FundProjectionScreenOutlined, ScheduleOutlined, ContactsOutlined,  BulbOutlined, MailOutlined } from '@ant-design/icons'
+import { GithubOutlined, FacebookOutlined, PhoneOutlined, EnvironmentOutlined, FundProjectionScreenOutlined, ScheduleOutlined, ContactsOutlined, BulbOutlined, MailOutlined } from '@ant-design/icons'
 import './index.css'
 
 
@@ -9,35 +9,32 @@ const FirstResume = () => {
 
     return (
         <div className="userResume_container">
-            <div className="leftSide_container">
+            <div className="first_profile_img">
                 <img src={profile.profileImage} alt="profile-image" />
-                <div className="contact">
-                    <h2><span><ContactsOutlined /></span>Contact</h2>
-                    <div>
-                        <h3><span className="home-icon"><EnvironmentOutlined /></span>{profile.adress}</h3>
-                        <h3><span><PhoneOutlined /></span>{profile.phoneNumber}</h3>
-                    </div>
-                </div>
-                <hr />
-                <div className="social">
-                    <h2><span><MailOutlined /></span>Socials</h2>
-                    <div>
-                        <a href={social.social_facebook} target="_blank" rel="noreferrer"><span className="facebook-icon-1"><FacebookOutlined /></span>FACEBOOK</a>
-                        <a href={social.social_github} target="_blank" rel="noreferrer"><span className="github-icon-1"><GithubOutlined /> </span>GITHUB</a>
-                    </div>
+            </div>
+            <div className="first_userName">
+                <h1>{profile.firstName} {profile.lastName}</h1>
+            </div>
+            <div className="contact">
+                <h2><span><ContactsOutlined /></span>Contact</h2>
+                <div>
+                    <h3><span className="home-icon"><EnvironmentOutlined /></span>{profile.adress}</h3>
+                    <h3><span><PhoneOutlined /></span>{profile.phoneNumber}</h3>
                 </div>
             </div>
-
-            <div className="rightSide_container">
-                <h1>{profile.firstName} {profile.lastName}</h1>
-                <hr />
-                <div className="education">
-                    <h2><span><ScheduleOutlined /></span>Education</h2>
-                    <div>
-                        {showEducations(education)}
-                    </div>
+            <div className="education">
+                <h2><span><ScheduleOutlined /></span>Education</h2>
+                <div>
+                    {showEducations(education)}
                 </div>
-                <hr />
+            </div>
+            <div className="social">
+                <h2><span><MailOutlined /></span>Socials</h2>
+                <div>
+                    <a href={social.social_facebook} target="_blank" rel="noreferrer"><span className="facebook-icon-1"><FacebookOutlined /></span>FACEBOOK</a>
+                    <a href={social.social_github} target="_blank" rel="noreferrer"><span className="github-icon-1"><GithubOutlined /> </span>GITHUB</a>
+                </div>
+            </div>
                 <div className="skills">
                     <h2><span><BulbOutlined /></span>Skills</h2>
                     <ul>
@@ -52,7 +49,6 @@ const FirstResume = () => {
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
 
