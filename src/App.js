0c1a1f3faa5a630d7a2bf49cd210b1072fb242eb
GrigoryanDav <grigoryand219@gmail.com>
2024-12-24 19:28:12 +0400

    fix bugs

--- src/App.js
+++ src/App.js
@@ -25,8 +25,7 @@ import './styles/global.css'
 
 
 const App = () => {
-  const { loading, authUserInfo: { isAuth, userData } } = useSelector((store) => store.userProfile)
-  const resume_sections = userData?.resume_sections
+  const { loading, authUserInfo: { isAuth, userData: { resume_sections } } } = useSelector((store) => store.userProfile)
   const dispatch = useDispatch()
 
   useEffect(() => {
